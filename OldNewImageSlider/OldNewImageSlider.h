279a75f35623995387199a39f6dd1a5fbedc2aa8
Aleksandar Atanackovic <aatanac@gmail.com>
2018-07-06 21:03:54 +0200

    Initial Commit

---
+++ OldNewImageSlider/OldNewImageSlider.h
@@ -0,0 +1,19 @@
+//
+//  OldNewImageSlider.h
+//  OldNewImageSlider
+//
+//  Created by Aleksandar Atanackovic on 06/07/2018.
+//  Copyright © 2018 Aleksandar Atanackovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OldNewImageSlider.
+FOUNDATION_EXPORT double OldNewImageSliderVersionNumber;
+
+//! Project version string for OldNewImageSlider.
+FOUNDATION_EXPORT const unsigned char OldNewImageSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OldNewImageSlider/PublicHeader.h>
+
+
